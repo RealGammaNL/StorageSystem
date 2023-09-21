1b4a0b13597fb4930fe77623a41eed5c00e66c0d
RealGammaNL <max@brosky.net>
2023-09-21 10:47:58 +0200

    Finished changing

---
+++ StorageAppMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StorageAppMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
